0b3c4e782e5bb054f911bff2b33a48629c939542
pfarah65 <pfara039@uottawa.ca>
2018-12-30 23:01:44 -0500

    sign up wrong pass msg

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -8,7 +8,7 @@ import {AuthService} from '../auth.service';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  invalidMatch = false;
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
@@ -16,7 +16,13 @@ export class SignupComponent implements OnInit {
   onSignUp(form: NgForm){
     const email = form.value.email;
     const password = form.value.password;
-    console.log(password);
-    this.authService.signupUser(email, password);
+    const passwordAgain = form.value.passwordAgain;
+    if(password === passwordAgain){
+
+      console.log(password);
+      this.authService.signupUser(email, password);
+    }else{
+      this.invalidMatch = true;
+    }
   }
 }
