a9ef182b3fb8a824d2fe3a591d7a7f60f40edffc
pfarah65 <pfara039@uottawa.ca>
2018-12-31 22:50:51 -0500

    auto login on refresh

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
+import {AuthService} from '../auth/auth.service';
+import {Router} from '@angular/router';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, DoCheck {
 
-  constructor() { }
+  constructor(private authService:AuthService,private router:Router) { }
 
   ngOnInit() {
+
+  }
+  ngDoCheck(){
+    if(firebase.auth().currentUser !== null){
+      firebase.auth().currentUser.getIdToken().then( (token) =>{
+        console.log('peter');
+        this.authService.hasToken(token);
+
+      });
+    }
   }
 
 }
